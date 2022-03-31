a70b9d013b3f1658dc8407a213d8e44a6d76fb19
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-03-31 16:03:02 -0500

    created click event, needs to be updated

--- javascript.js
+++ javascript.js
@@ -12,9 +12,23 @@ function playSound(event) {
 }
 
 const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", click);
+});
 
 function removeTransition(event) {
   if (event.propertyName !== "transform") return;
   this.classList.remove("playing");
+}
+
+function click(event) {
+  const audio = document.querySelector(`audio[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
+  const key = document.querySelector(`.key[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
+  if (!audio || !key) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("playing");
 }
