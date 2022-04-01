42c3139ecc8f06ee3dd846b3af8cd0c05c1cb069
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-03-31 20:59:40 -0500

    separate playSound into three: playKey, playClick, playSound

--- javascript.js
+++ javascript.js
@@ -1,34 +1,32 @@
-window.addEventListener("keydown", playSound);
-
-function playSound(event) {
+function playKey(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio || !key) return;
-
-  audio.currentTime = 0;
-  audio.play();
-
-  key.classList.add("playing");
+  playSound(audio, key);
 }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
-  key.addEventListener("click", click);
-});
+function playClick(event) {
+  const audio = document.querySelector(`audio[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
+  const key = document.querySelector(`.key[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
+  if (!audio || !key) return;
+  playSound(audio, key);
+}
+
+function playSound(audio, key) {
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
 
 function removeTransition(event) {
   if (event.propertyName !== "transform") return;
   this.classList.remove("playing");
 }
 
-function click(event) {
-  const audio = document.querySelector(`audio[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
-  const key = document.querySelector(`.key[data-key="${event.path[1].attributes["data-key"].nodeValue}"]`);
-  if (!audio || !key) return;
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", playClick);
+});
 
-  audio.currentTime = 0;
-  audio.play();
-
-  key.classList.add("playing");
-}
+window.addEventListener("keydown", playKey);
